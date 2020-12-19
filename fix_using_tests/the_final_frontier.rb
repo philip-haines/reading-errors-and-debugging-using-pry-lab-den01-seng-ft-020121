02eaeec07e919e1ec9f92a5d55ec4be64be2348a
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-19 00:29:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,15 +2,16 @@
 
 require "pry"
 
-def generate_star_date
- star_date = (rand(100000) + 400000) / 10.0
-end
-
-def state_log(generate_star_date)
-  "Captain's Log, star date #{star_date}."
-end
-
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  def generate_star_date
+    star_date = (rand(100000) + 400000) / 10.0
+  end
+
+  def state_log(star_date)
+    puts "Captain's Log, star date #{star_date}."
+  end
+
+  state_log(generate_star_date)
 end
+
+engage
